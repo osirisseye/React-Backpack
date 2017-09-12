de5f73858b5dfacc0f0d59611f163bbd32c85dae
Lukasz <lukaszandruchow@gmail.com>
2017-09-12 13:48:54 +0100

    before migration

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
 import App from 'App';
 
-it('App', () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+
+describe('App', () => {
+    it('renders without crashing', () => {
+            mount(<App />);
+    });
+
+    it('App', () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<App />, div);
+    });
 });
+
 
